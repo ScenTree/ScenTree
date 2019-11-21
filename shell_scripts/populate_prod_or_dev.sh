544f46e97c595b484f84208c08014367f82deddb
djakk <djakk.djakk@gmail.com>
2019-11-21 17:27:22 +0100

    changed hard-coded paths

--- shell_scripts/populate_prod_or_dev.sh
+++ shell_scripts/populate_prod_or_dev.sh
@@ -1,14 +1,20 @@
 # 
 
+THE_SECRET_PSWRD # if env var is set, continue
 
-# dev environment : github is inside : /home/maxime/dev_BDD/ScenTree 
+# get the Scentree folder from the path of the shell script
+
+# cp from the Scentree folder to dev_BDD's folders (style, html - no nodejs_scripts in dev env) and to solr conf folder
+# also set password inside style's datasource conf file
+
+
+
 # main.js gloval var set to "dev", 
 # ln -s pre_index.html index.html, 
 # delete old config of solr dev_taxo, copy new config inside solr dev_taxo, 
 # delete old data in solr dev_taxo, post new data in solr dev_taxo,
 # delete old osm-style xml from dev-mapnik, copy new osm-style xml to dev-mapnik
 
-THE_SECRET_PSWRD # if env var is set, continue
 MY_SCENTREE_ENVIRONMENT="dev"
 python3 /home/maxime/dev_BDD/ScenTree/python_script/lm-travtree.py
 
@@ -18,9 +24,9 @@ su - scentree -c "\
 	curl http://localhost:8983/solr/dev_taxoEN/update?commit=true -d '<delete><query>*:*</query></delete>;\
 	curl http://localhost:8983/solr/dev_taxoFR/update?commit=true -d '<delete><query>*:*</query></delete>';\
 	curl http://localhost:8983/solr/dev_taxoENandFR/update?commit=true -d '<delete><query>*:*</query></delete>';\
-	~/src/solr-8.1.1/bin/post -c dev_taxoEN /home/maxime/dev_BDD/TreeFeaturesNEW_EN.json;\
-	~/src/solr-8.1.1/bin/post -c dev_taxoFR /home/maxime/dev_BDD/TreeFeaturesNEW_FR.json;\
-	~/src/solr-8.1.1/bin/post -c dev_taxoENandFR /home/maxime/dev_BDD/TreeFeaturesNEW_EN_and_FR.json\
+	~/src/solr-8.1.1/bin/post -c dev_taxoEN /home/maxime/dev_BDD/BDD/TreeFeaturesNEW_EN.json;\
+	~/src/solr-8.1.1/bin/post -c dev_taxoFR /home/maxime/dev_BDD/BDD/TreeFeaturesNEW_FR.json;\
+	~/src/solr-8.1.1/bin/post -c dev_taxoENandFR /home/maxime/dev_BDD/BDD/TreeFeaturesNEW_EN_and_FR.json\
 	"
 
 
