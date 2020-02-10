b1df9e1b4e69ac4b27c47bb9393d1c1fe964b2b8
Julein "djakk <julien.tadashi@gmail.com>
2020-02-10 10:12:04 +0100

    Immediate effect on the notification item for the survey

--- html/js/main.js
+++ html/js/main.js
@@ -74,6 +74,7 @@ if (! window.document.jsdom_reader) {
 
 $(".survey_link").click(function(){
   Cookies.set('Updated_on__the_survey', UPDATED_ON["the survey"], { expires: 365 });
+  $(".notifi4").css({'display' : 'none'});
 });
 
 $("#no_cookie").click(function(){
