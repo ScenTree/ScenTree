2d710a1a8606e3c7bebe1bfd0adeea25e27a3b5a
ScenTree <56188051+ScenTree@users.noreply.github.com>
2020-03-08 11:53:09 +0100

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -721,7 +721,7 @@ function markofun(the_node_as_json_EN_and_FR, show_the_modal = true) {
       for (let an_infra_info of the_ifra_infos) {
         var the_ifra_info = JSON.parse(an_infra_info);
         the_new_ifra_infos.push(the_ifra_info);
-        console.log(the_ifra_info);
+        //console.log(the_ifra_info);
       };
     };
    function afficheElement(element, index, array) {
