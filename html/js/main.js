727cec63938f96439c339b3a8ab406805b1f254b
djakk <djakk.djakk@gmail.com>
2020-03-15 12:10:09 +0100

    new condition to show the listy IFRA tables

--- html/js/main.js
+++ html/js/main.js
@@ -629,7 +629,7 @@ function markofun(the_node_as_json_EN_and_FR, show_the_modal = true) {
 		show_the_skin_IFRA_table = true;
 	};
 
-	if (the_ifra_info["Level (%)"] && (! the_ifra_info["Botanical Binomial name"])) {
+	if (the_ifra_info["Level (%)"] && is_an_naturelle) {
                 if (the_ifra_info["version"].indexOf("48") >= 0) {
                         the_current_row = $(".ingredients-containing-without-botanicals-48").append($("<tr></tr>"));
                         the_current_row.append($("<td></td>").text(the_ifra_info["Principle Name2"]));
@@ -653,7 +653,7 @@ function markofun(the_node_as_json_EN_and_FR, show_the_modal = true) {
 		};	
 	};
 	
-	if (the_ifra_info["Botanical Binomial name"]) {
+	if (the_ifra_info["Level (%)"] && (! is_an_naturelle)) {
 		if (the_ifra_info["version"].indexOf("48") >= 0) {
 			the_current_row = $(".ingredients-containing-with-botanicals-48").append($("<tr></tr>"));
 			the_current_row.append($("<td></td>").text(the_ifra_info["Principle Name2"]));
