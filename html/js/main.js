ffdff3ffc287282628306242ed145e314c4de2d4
ScenTree <56188051+ScenTree@users.noreply.github.com>
2020-04-23 22:34:24 +0200

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -809,7 +809,7 @@ function markofun(the_node_as_json_EN_and_FR, show_the_modal = true) {
     };
     };
 
-if (the_ifra_info["version"] != "Annexe Type II 49TH") {
+if (the_ifra_info["version"] == "Annexe Type II 49TH") {
     $(".IFRA-infos").hide();
   };
     //$(".IFRA .container").show();
