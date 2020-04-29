1b19d9bccb258a1189ee7ceaf4c6f6fb465ea5fa
djakk <djakk.djakk@gmail.com>
2020-04-29 17:54:23 +0200

    added the link to the sponsor page

--- html/js/main.js
+++ html/js/main.js
@@ -549,8 +549,9 @@ function fill_with_percentage(the_html_class_as_text, the_percentage) {
 };
 
 function get_the_pro_img_from_the_pro_info(the_pro_info) {
-     var the_html_div = $("<div></div>")
-	.addClass("m-2 with-relative-position");
+     var the_html_div = $("<a></a>")
+	.addClass("d-block m-2 with-relative-position")
+	.attr("href", "../sponsors/" + the_pro_info["Nom Tiers"] + ".html");
      var the_main_html_img_in_french = $("<img />");
      the_main_html_img_in_french.attr("src", "/img/sponsors/sponsor_example_1.jpeg")
 	.addClass("img-fluid")
