462467fd29dd89bcd451ed32232ba7c4a2178d1c
ScenTree <56188051+ScenTree@users.noreply.github.com>
2020-05-04 15:17:05 +0200

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -959,7 +959,7 @@ function markofun(the_node_as_json_EN_and_FR, show_the_modal = true) {
       var the_container_for_the_standard_pros = $("<div></div>").addClass("top-content").append(
                   $("<div></div>").addClass("container-fluid").append(
                       $("<div></div>").addClass("glide carouselcentré myCarousel").append(
-                              $("<div></div>").addClass("glide__track").attr("data-glide-el", "track").append(
+                              $("<div></div>").addClass("glide__track mt-1 mb-1").attr("data-glide-el", "track").append(
                                       $("<ul></ul>").addClass("glide__slides")
                               )
                       )
