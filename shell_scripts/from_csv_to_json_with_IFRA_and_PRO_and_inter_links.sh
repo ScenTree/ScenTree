36d6000ea539dac760a31d9dcc11b8460e3e34c2
djakk <djakk.djakk@gmail.com>
2020-09-06 09:23:09 +0200

    added the control on the return code of the python scripts

--- shell_scripts/from_csv_to_json_with_IFRA_and_PRO_and_inter_links.sh
+++ shell_scripts/from_csv_to_json_with_IFRA_and_PRO_and_inter_links.sh
@@ -7,16 +7,23 @@ then
 	PLEASE_STOP=1
 fi
 
-THE_FILES_FOLDER="../files"
-THE_PYTHON_SCRIPTS_FOLDER="../ScenTree/python_script"
+THE_FILES_FOLDER="../../files"
+THE_PYTHON_SCRIPTS_FOLDER="../python_script"
 THE_ENVIRONMENT="$1"
 
-if [ -f "$THE_FILES_FOLDER" ]
+if [ -z "$THE_ENVIRONMENT" ]
+then
+	echo "Please set a first argument (the environment, prod or dev), aborting"
+        PLEASE_STOP=1
+fi
+
+
+if [ ! -d "$THE_FILES_FOLDER" ]
 then
 	echo "Folder '$THE_FILES_FOLDER' not found !"
 	PLEASE_STOP=1
 fi
-if [ -f "$THE_PYTHON_SCRIPTS_FOLDER" ]
+if [ ! -d "$THE_PYTHON_SCRIPTS_FOLDER" ]
 then
         echo "Folder '$THE_PYTHON_SCRIPTS_FOLDER' not found !"
         PLEASE_STOP=1
@@ -33,17 +40,17 @@ THE_LM_TRAVTREE_SCRIPT="$THE_PYTHON_SCRIPTS_FOLDER/lm-travtree.py"
 THE_PRO_OR_IFRA_SCRIPT="$THE_PYTHON_SCRIPTS_FOLDER/from_pro_csv_to_updated_jsons.py"
 THE_INTER_LINKS_SCRIPT="$THE_PYTHON_SCRIPTS_FOLDER/do_inter_links.py"
 
-if [ -f "$THE_LM_TRAVTREE_SCRIPT" ]
+if [ ! -f "$THE_LM_TRAVTREE_SCRIPT" ]
 then
         echo "Script '$THE_LM_TRAVTREE_SCRIPT' not found !"
         PLEASE_STOP=1
 fi
-if [ -f "$THE_PRO_OR_IFRA_SCRIPT" ]
+if [ ! -f "$THE_PRO_OR_IFRA_SCRIPT" ]
 then
         echo "Script '$THE_PRO_OR_IFRA_SCRIPT' not found !"
         PLEASE_STOP=1
 fi
-if [ -f "$THE_INTER_LINKS_SCRIPT" ]
+if [ ! -f "$THE_INTER_LINKS_SCRIPT" ]
 then
         echo "Script '$THE_INTER_LINKS_SCRIPT' not found !"
         PLEASE_STOP=1
@@ -64,19 +71,48 @@ echo "Files like 'TreeFeaturesNEW_EN_and_FR.json' inside the files folder ('$THE
 
 echo "Lauching lm-travtree …"
 python3 "$THE_LM_TRAVTREE_SCRIPT" "$THE_ENVIRONMENT" "$THE_FILES_FOLDER"
-echo "lm-travtree done !"
+THE_CODE_OF_THE_SCRIPT="$?"
+if [ "$THE_CODE_OF_THE_SCRIPT" -ne 0 ]
+then
+	echo "lm-travtree : problem ! aborting …"
+	exit 1
+else
+	echo "lm-travtree : done !"
+fi
 
 echo "Lauching from_pro_csv_to_updated_jsons.py (IFRA) …"
 python3 "$THE_PRO_OR_IFRA_SCRIPT" "$THE_FILES_FOLDER/CSV_IFRA.csv" "$THE_FILES_FOLDER/TreeFeaturesNEW_EN_and_FR_original.json" "IFRA" > "$THE_FILES_FOLDER/TreeFeaturesNEW_EN_and_FR__with_IFRA.json"
-echo "from_pro_csv_to_updated_jsons.py (IFRA) done !"
+THE_CODE_OF_THE_SCRIPT="$?"
+if [ "$THE_CODE_OF_THE_SCRIPT" -ne 0 ]
+then
+        echo "from_pro_csv_to_updated_jsons.py (IFRA) : problem ! aborting …"
+        exit 1
+else
+	echo "from_pro_csv_to_updated_jsons.py (IFRA) done !"
+fi
+
 
 echo "Lauching from_pro_csv_to_updated_jsons.py (PRO) …"
 python3 "$THE_PRO_OR_IFRA_SCRIPT" "$THE_FILES_FOLDER/CSV_PRO.csv" "$THE_FILES_FOLDER/TreeFeaturesNEW_EN_and_FR__with_IFRA.json" "PRO" > "$THE_FILES_FOLDER/TreeFeaturesNEW_EN_and_FR__with_IFRA_and_PRO.json"
-echo "from_pro_csv_to_updated_jsons.py (PRO) done !"
+THE_CODE_OF_THE_SCRIPT="$?"
+if [ "$THE_CODE_OF_THE_SCRIPT" -ne 0 ]
+then
+        echo "from_pro_csv_to_updated_jsons.py (PRO) : problem ! aborting …"
+        exit 1
+else
+        echo "from_pro_csv_to_updated_jsons.py (PRO) done !"
+fi
 
 echo "Lauching do_inter_links.py …"
 python3 "$THE_INTER_LINKS_SCRIPT" "$THE_FILES_FOLDER/TreeFeaturesNEW_EN_and_FR__with_IFRA_and_PRO.json" > "$THE_FILES_FOLDER/TreeFeaturesNEW_EN_and_FR.json"
-echo "do_inter_links.py done !"
+THE_CODE_OF_THE_SCRIPT="$?"
+if [ "$THE_CODE_OF_THE_SCRIPT" -ne 0 ]
+then
+        echo "do_inter_links.py : problem ! aborting …"
+        exit 1
+else
+	echo "do_inter_links.py done !"
+fi
 
 
 exit 0
