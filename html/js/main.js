64a164296f6141dac3d69b3871a8d32bf65fb523
ScenTree <56188051+ScenTree@users.noreply.github.com>
2020-09-07 18:49:34 +0200

    Update main.js

--- html/js/main.js
+++ html/js/main.js
@@ -1227,7 +1227,7 @@ function markofun(the_node_as_json_EN_and_FR, show_the_modal = true) {
         $('#modalbody-pict').empty();
         $('#modalbody-pictmap').empty();
         $('#modalbody-pict').append("<img class='imgmp' src=\"../img/matieres_premieres/" + the_img_title + ".jpg\" alt=\"" + the_webpage_title + "\" title=\"" + the_webpage_title + "\" />");
-        $('#modalbody-pictmap').append("<img class='imgmp' src=\"../img/matieres_premieres/map/" + the_img_title + ".PNG\" alt=\"" + the_webpage_title + "\" title=\"" + the_webpage_title + "\" />"); 
+        $('#modalbody-pictmap').append("<img class='imgmap' src=\"../img/matieres_premieres/map/" + the_img_title + ".PNG\" alt=\"" + the_webpage_title + "\" title=\"" + the_webpage_title + "\" />"); 
         if (show_the_modal) $('#naturelleModal').modal('show');
     };
     if (is_an_synthetique) {
@@ -1235,7 +1235,7 @@ function markofun(the_node_as_json_EN_and_FR, show_the_modal = true) {
         $('#modalbody-pict1').empty();
         $('#modalbody-pict1A').empty();
         $('#modalbody-pict1').append("<img class='imgmp' src=\"../img/matieres_premieres/" + the_img_title + ".PNG\" alt=\"" + the_webpage_title + "\" title=\"" + the_webpage_title + "\" />");
-        $('#modalbody-pict1A').append("<img class='imgmap' src=\"../img/matieres_premieres/" + the_img_title + ".PNG\" alt=\"" + the_webpage_title + "\" title=\"" + the_webpage_title + "\" />");
+        $('#modalbody-pict1A').append("<img class='imgmp' src=\"../img/matieres_premieres/" + the_img_title + ".PNG\" alt=\"" + the_webpage_title + "\" title=\"" + the_webpage_title + "\" />");
         if (show_the_modal) $('#SynthetiqueModal').modal('show');
     };
     if (is_an_descripteur) {
