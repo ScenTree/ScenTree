c2945b8bfd4a037d8c6dfd32e95336b2e5d3b4d0
djakk <djakk.djakk@gmail.com>
2020-09-17 14:31:59 +0200

    differencing URL according to their language in 'do_inter_link'

--- python_script/commons.py
+++ python_script/commons.py
@@ -5,7 +5,7 @@
 def formatting_text_for_a_webpage_address(the_text):
     return the_text.replace(" ", "_").replace("/", "_").replace("'", "_").replace('"', "_")
 
-def compute_the_webpage_address(the_EN_name, the_FR_name):
-    return "../ingredients/%s__%s.html" % (formatting_text_for_a_webpage_address(the_EN_name), formatting_text_for_a_webpage_address(the_FR_name))
+def compute_the_webpage_address(the_EN_name, the_FR_name, the_language):
+    return "../" + the_language + "/ingredients/%s__%s.html" % (formatting_text_for_a_webpage_address(the_EN_name), formatting_text_for_a_webpage_address(the_FR_name))
 
 
